a5ec13318532e38bdf10337180202f6b63b5b6c0
Bluebaritone21 <82056947+Bluebaritone21@users.noreply.github.com>
2025-03-26 08:06:20 -0700

    Now, /this/ should work.

--- apps/texteditor.js
+++ apps/texteditor.js
@@ -10,6 +10,8 @@ function texteditor(){
     title.style.position='absolute';
     title.style.top='-5px';
     title.style.textAlign="center";
+    title.style.width="80%";
+    title.style.left="10%";
     win.appendChild(title);
 }
 
