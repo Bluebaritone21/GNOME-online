9c57c53d498f23727fdb705f16aa04141a40b06d
Bluebaritone21 <82056947+Bluebaritone21@users.noreply.github.com>
2025-03-27 09:46:57 -0700

    optimized the battery program.

--- wm.js
+++ wm.js
@@ -122,6 +122,7 @@ function time(){
 setInterval(time,1000);
 
 navigator.getBattery().then(function(battery) {
+  function updateIcon(){
   var battery_button = $("battery-button");
   battery_button.title = battery.level * 100 + "%";
   if(battery.level == 1){
@@ -137,23 +138,10 @@ navigator.getBattery().then(function(battery) {
       battery_button.classList.remove("white-icon");
       battery_button.src = getIcon("battery-action");
   }
+  }
+  updateIcon();
   // ... and any subsequent updates.
-  battery.onlevelchange = function() {
-      battery_button.title = battery.level * 100 + "%";
-      if(battery.level == 1){
-        battery_button.classList.add("white-icon");
-        battery_button.src= getIcon("battery-level-100");
-      }else if(battery.level >=0.3){
-        battery_button.classList.add("white-icon");
-        battery_button.src = getIcon("battery-level-"+Math.round(battery.level*10)*10);
-      }else if(battery.level >= 0.2){
-        battery_button.classList.add("white-icon");
-        battery_button.src = getIcon("battery-low");
-      }else{
-        battery_button.classList.remove("white-icon");
-        battery_button.src = getIcon("battery-action");
-    }
-  };
+  battery.onlevelchange = updateIcon;
 });
 
 function $(id){
