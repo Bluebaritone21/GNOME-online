d3b3186a371b7022b3a82c5045070cd66b5ea888
Bluebaritone21 <82056947+Bluebaritone21@users.noreply.github.com>
2025-03-27 09:19:02 -0700

    (Nonfunctional) Dark mode button

--- apps/calculator.js
+++ apps/calculator.js
@@ -2,7 +2,7 @@ function calculator(){
     var warning = document.createElement("h1");
     warning.classList.add("fg-warning");
     warning.innerText = "Comming Soon";
-    makeWindow(200,300,warning)
+    makeWindow(310,442,warning)
 }
 
 registerApp("https://apps.gnome.org/icons/scalable/org.gnome.Calculator.svg",calculator,"Calculator");
