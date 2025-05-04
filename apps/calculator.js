3eed26bfe3b1a6e9d6f37c373e9ff8d0ef94fc22
Bluebaritone21 <82056947+Bluebaritone21@users.noreply.github.com>
2025-05-04 22:50:29 +0000

    Moved the table to take up as much room as it can.

--- apps/calculator.js
+++ apps/calculator.js
@@ -14,6 +14,8 @@ function calculator(){
     function evalMaths(maths){
     }
     var buttonTable = UIelmnt("table");
+    resize(buttonTable,"100%","calc(100% - 85px)");
+    repos(buttonTable,5,85);
     for(var rowI = 0; rowI<buttons.length;rowI++){
         var row = UIelmnt("tr");
         for(var cellI = 0; cellI<buttons[rowI].length; cellI++){
