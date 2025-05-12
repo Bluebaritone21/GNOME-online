03621beabf08cb6db539e8272cb050aad7731cb9
Bluebaritone21 <82056947+Bluebaritone21@users.noreply.github.com>
2025-05-12 16:00:13 +0000

    Forgot to turn off delWorkspace. Oops :/

--- wm.js
+++ wm.js
@@ -434,7 +434,7 @@ class Workspace {
         this.button.innerText = this.i;
         $('workspace-buttons').appendChild(this.button);
         let i = this.i;
-        this.button.onclick = function () { if(isDelWork){delWorkspace(i)}else{switchWorkspace(i)} };
+        this.button.onclick = function () { if(isDelWork){delWorkspace(i);isDelWork = false;}else{switchWorkspace(i)} };
     }
 }
 
